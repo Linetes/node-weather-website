2ff4fe67d6c9d6b7c96a0e8b31a135c417d13ea7
Linetes <linetescesar@gmail.com>
2022-01-25 01:53:34 -0600

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,10 @@
-console.log('Client side javascript file is loaded!')
-
 fetch('http://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data) => {
         console.log(data)
     })
 }) 
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
+fetch('/weather?address=boston').then((response) => {
     response.json().then((data) => { {}
         if(data.error) {
             console.log(data.error);
@@ -15,7 +13,7 @@ fetch('http://localhost:3000/weather?address=boston').then((response) => {
             console.log(data.forecast)
         }
     })
-}) 
+})
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -28,7 +26,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
     messageOne.textContent = 'Loading...'
 
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('/weather?address='+location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error);
